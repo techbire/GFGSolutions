6c035f2d68c684bb3a397a37ed89f9d052913012
Ansh Gupta <techbire69@gmail.com>
2024-09-10 19:37:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Modular Node/modular-node.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*Struture of the node of the linked list is as:
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+// Your task is to complete the function
+// function should return the modular Node
+// if no such node is present then return -1
+class Solution {
+
+   public:
+    int modularNode(Node *head, int k) {
+        if (k <= 0) return -1;
+
+        int ans = -1;
+        int index = 1;
+        Node *curr = head;
+
+        while (curr != NULL) {
+            if (index % k == 0) {
+                ans = curr->data;
+            }
+            index++;
+            curr = curr->next;
+        }
+
+        return ans;}
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        int res = ob.modularNode(head, k);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

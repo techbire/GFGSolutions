d9837d1014f589c41658beff5ea5474fe9182ef1
Ansh Gupta <techbire69@gmail.com>
2024-10-16 19:02:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Swaps/two-swaps.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        // code here.
+        int count=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            while(arr[i]!=i+1){
+                swap(arr[i],arr[arr[i]-1]);
+                count++;
+                i--;
+            }
+            if(count>2)return false;
+        }
+        return count==2||count==0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        bool ans = ob.checkSorted(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+}
+
+// } Driver Code Ends

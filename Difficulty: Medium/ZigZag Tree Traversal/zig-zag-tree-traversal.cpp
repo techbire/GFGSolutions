0e8fcff68d23b356859b562008186d3f662e5a3d
Ansh Gupta <techbire69@gmail.com>
2025-10-10 23:51:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.cpp
@@ -0,0 +1,36 @@
+/*
+class Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> zigZagTraversal(Node* root) {
+        vector<int> ans;
+        queue<Node*> q;
+        q.push(root);
+        bool f = 0;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> v;
+            for(int i = 0;i<size;i++){
+                Node* front = q.front(); q.pop();
+                v.push_back(front->data);
+                
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+            }
+            if(f) reverse(v.begin(), v.end());
+            f =!f;
+            ans.insert(ans.end(), v.begin(), v.end());
+        }
+        return ans;
+    }
+};

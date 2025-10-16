c962acaca4694a7f2c5792b02a71ba0012578105
Ansh Gupta <techbire69@gmail.com>
2025-10-16 23:26:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove BST keys outside given range/remove-bst-keys-outside-given-range.cpp
@@ -0,0 +1,47 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution {
+    void prune(Node* root, int l, int r, Node* parent) {
+        if(!root) return;
+        if(root->data > r){
+            root->right = NULL;
+            if(parent->data < root->data){
+                parent->right = root->left;
+            }else{
+                parent->left = root->left;
+            }
+            prune(root->left, l, r, parent);
+        }
+        else if(root->data < l){
+            root->left = NULL;
+            if(parent->data < root->data){
+                parent->right = root->right;
+            }else{
+                parent->left = root->right;
+            }
+            prune(root->right, l, r, parent);
+        }else{
+            prune(root->left, l, r, root);
+            prune(root->right, l, r, root);
+        }
+    }
+  public:
+    Node* removekeys(Node* root, int l, int r) {
+        Node * froot = new Node(INT_MAX);
+        froot->left = root;
+        prune(root, l, r, froot);
+        return froot->left;
+    }
+};

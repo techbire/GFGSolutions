396e1de8dd2478a9ce6f92d209fd914cd360b256
Ansh Gupta <techbire69@gmail.com>
2026-01-08 01:37:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  int count(vector<int>& nums, int k){
+        int ans = 0;
+        int odd = 0;
+        int i = 0;
+
+        for(int j = 0;j<nums.size();j++){
+            if(nums[j]%2!=0) odd++;
+
+            while(odd>k){
+                if(nums[i]%2!=0) odd--;
+                i++;
+            }
+
+            ans += (j -i+1);
+        }
+
+        return ans;}
+
+    int countSubarrays(vector<int>& arr, int k) {
+        // code here
+         return count(arr,k) - count(arr,k-1);
+    }
+};

962f795446ded9419c3513775cd49b61c1571f4e
Ansh Gupta <techbire69@gmail.com>
2026-05-16 22:56:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Not a subset sum/not-a-subset-sum.cpp
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.cpp
@@ -1,49 +1,16 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    long long findSmallest(vector<int> &arr) {
-        // Your code goes here.
-        long long ans = 1;
-        if(arr[0] > 1)return 1;
-        for(auto itr : arr){
-            if(itr > ans)return ans;
-            ans += itr;
+    int findSmallest(vector<int> &arr) {
+        // code here
+    sort(arr.begin(),arr.end());
+    int check=1;
+    for(int i=0;i<arr.size();i++){
+        if(check<arr[i]){
+            return check;
         }
-        return ans;
+        else{
+            check+=arr[i];
+        }}
+        return check;
     }
-        
-     
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-
-        Solution ob;
-        auto ans = ob.findSmallest(arr);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends

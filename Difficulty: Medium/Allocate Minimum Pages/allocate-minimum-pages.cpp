133c044c848f1e90374c9d36227f28377f10e4ae
Ansh Gupta <techbire69@gmail.com>
2026-06-21 18:50:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,37 +1,21 @@
 class Solution {
-  public:
-    bool isPossible(vector<int> &arr, int k, int maxPages){
-            int students = 1, pageSum = 0;
-            for(int i=0;i<arr.size();i++){
-                pageSum += arr[i];
-                if(pageSum > maxPages){
-                    students++;
-                    pageSum = arr[i];
-                }
-                if(students > k)
-                    return false;
+public:
+    int findPages(vector<int>& a, int k) {
+        if (a.size() < k) return -1;
+
+        int l = *max_element(a.begin(), a.end());
+        int r = accumulate(a.begin(), a.end(), 0);
+
+        while (l < r) {
+            int m = l + (r - l) / 2, s = 0, c = 1;
+
+            for (int x : a) {
+                if (s + x > m) c++, s = x;
+                else s += x;
             }
-            return true;
+
+            c <= k ? r = m : l = m + 1;
         }
-  
-    int findPages(vector<int> &arr, int k) {
-        if(k > arr.size()) return -1;
-        
-        int start = 0, end = 0, res = -1;
-        for(int i=0;i<arr.size();i++){
-            start = max(start, arr[i]);
-            end += arr[i];
-        }
-        
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(isPossible(arr, k, mid)){
-                res = mid;
-                end = mid - 1;
-            }
-            else
-                start = mid + 1;
-        }
-        return res;
+        return l;
     }
 };
